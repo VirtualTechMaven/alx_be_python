9e7bbd5f003e7d0fe96f9d212b45e43259e21643
Chioma Princess Ohwo <ohwoprincess@gmail.com>
2025-07-23 23:54:53 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,15 @@
 def perform_operation(num1: float, num2: float, operation: str):
     """
-    Performs basic arithmetic operations between two numbers.
+    Performs basic arithmetic operations: add, subtract, multiply, divide.
 
     Parameters:
     - num1 (float): The first number
     - num2 (float): The second number
-    - operation (str): The operation to perform: 'add', 'subtract', 'multiply', or 'divide'
+    - operation (str): The operation to perform
 
     Returns:
-    - The result of the arithmetic operation
-    - Or a specific message in case of division by zero
+    - float result of the operation
+    - str message for invalid operation or division by zero
     """
     if operation == 'add':
         return num1 + num2
@@ -19,7 +19,7 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            return "Error: Division by zero is not allowed."
+            return "Error: Cannot divide by zero"
         return num1 / num2
     else:
-        return "Error: Invalid operation."
+        return "Error: Invalid operation"
